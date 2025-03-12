d8becfcb49a8679af99153246162328a4ea3f011
Kento Yamamoto <key7135g@gmail.com>
2025-03-12 17:00:19 +0900

    made article page

--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qiita_me/models/article.dart';
 import 'package:intl/intl.dart';
+import 'package:qiita_me/screens/article_screen.dart';
 
 class ArticleContainer extends StatelessWidget {
   const ArticleContainer({super.key, required this.article});
@@ -11,71 +12,87 @@ class ArticleContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        decoration: const BoxDecoration(
-          color: Color(0xFF55C500),
-          borderRadius: BorderRadius.all(Radius.circular(16)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              DateFormat('yyyy/MM/dd').format(article.createdAt),
-              style: const TextStyle(color: Colors.white, fontSize: 12),
+      child: GestureDetector(
+        // GestureDetectorでContainerを囲う
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: ((context) => ArticleScreen(article: article)),
             ),
-            Text(
-              article.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          decoration: const BoxDecoration(
+            color: Color(0xFF55C500),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat('yyyy/MM/dd').format(article.createdAt),
+                style: const TextStyle(color: Colors.white, fontSize: 12),
               ),
-            ),
-            Text(
-              '#${article.tags.join(' #')}', // ←文字列の配列をjoinで結合
-              style: const TextStyle(
-                fontSize: 12,
-                color: Colors.white,
-                fontStyle: FontStyle.italic, // ←フォントスタイルを斜体に変更
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // ハートアイコンといいね数
-                Column(
-                  children: [
-                    const Icon(Icons.favorite, color: Colors.white),
-                    Text(
-                      article.likesCount.toString(),
-                      style: const TextStyle(fontSize: 12, color: Colors.white),
-                    ),
-                  ],
+              Text(
+                article.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                // 投稿者のアイコンと投稿者名
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      radius: 26,
-                      backgroundImage: NetworkImage(
-                        article.user.profileImageUrl,
+              ),
+              Text(
+                '#${article.tags.join(' #')}', // ←文字列の配列をjoinで結合
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic, // ←フォントスタイルを斜体に変更
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // ハートアイコンといいね数
+                  Column(
+                    children: [
+                      const Icon(Icons.favorite, color: Colors.white),
+                      Text(
+                        article.likesCount.toString(),
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      article.user.id,
-                      style: const TextStyle(fontSize: 12, color: Colors.white),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                  // 投稿者のアイコンと投稿者名
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 26,
+                        backgroundImage: NetworkImage(
+                          article.user.profileImageUrl,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        article.user.id,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
